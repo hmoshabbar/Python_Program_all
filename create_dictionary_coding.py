935dcc6da4be5c7d26fa87ffba0caaab191aaf83
Moshabbar Hussain <talktomoshabbar@gmail.com>
2017-07-26 18:56:06 +0530

    creating Dictionary program

---
+++ create_dictionary_coding.py
@@ -0,0 +1,25 @@
+import json
+class Main(dict):
+    def __init__(self):
+        self=dict()
+    def Create_dict(self,key,value):
+        self[key]=value
+
+
+Dict=[]        
+new_dict=Main()
+new_dict.Create_dict("Name","Moshabbar")
+new_dict.Create_dict("Age",28)
+new_dict.Create_dict("Sex","Male")
+#print new_dict
+json_output=json.dumps(new_dict)
+print json_output
+Dict.append(json_output)
+print Dict
+
+if __name__=="__main__":
+    Main()
+
+
+    
+

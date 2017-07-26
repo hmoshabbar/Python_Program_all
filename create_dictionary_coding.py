b2eb3527c0704f600e41bd1f2aa1e59e71df11e9
Moshabbar Hussain <talktomoshabbar@gmail.com>
2017-07-26 19:04:03 +0530

    Update create_dictionary_coding.py

--- create_dictionary_coding.py
+++ create_dictionary_coding.py
@@ -20,6 +20,33 @@ print Dict
 if __name__=="__main__":
     Main()
 
-
     
+    
+    
+    
+    
+ANOTHER WAY TO DO THIS CODING.......  
+
+import json
+key=raw_input("Enter Your Key:")
+value=raw_input("Enter Your Value:")
+class Main(dict):
+    def __init__(self):
+        self=dict()
+    def Create_dict(self,key,value):
+        self[key]=value
+
+
+Dict=[]        
+new_dict=Main()
+new_dict.Create_dict(key,value)
+
+#print new_dict
+json_output=json.dumps(new_dict)
+print json_output
+Dict.append(json_output)
+print Dict
+
+if __name__=="__main__":
+    Main()
 
